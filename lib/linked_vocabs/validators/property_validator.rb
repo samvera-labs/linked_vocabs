149e184341a4e6e67eaeb0347dd1130a17965cf0
Justin Coyne <jcoyne@justincoyne.com>
2016-01-08 10:14:19 -0600

    Give an error when a literal is set
    This is helpful if someone tries to set a string containing a URI and
    gets an error.  The real error is that they didn't cast the string to a
    URI.

--- lib/linked_vocabs/validators/property_validator.rb
+++ lib/linked_vocabs/validators/property_validator.rb
@@ -7,7 +7,11 @@ module LinkedVocabs::Validators
         unless v.try(:in_vocab?)
           term = v.try(:rdf_subject) || v
           vocabularies = record.class.properties[attribute.to_s].class_name.vocabularies.keys
-          record.errors.add :base, "value `#{term}' for `#{attribute}' property is not a term in a controlled vocabulary #{vocabularies.join(', ')}"
+          if term.is_a? RDF::URI
+            record.errors.add :base, "value `#{term}' for `#{attribute}' property is not a term in a controlled vocabulary #{vocabularies.join(', ')}"
+          else
+            record.errors.add :base, "`#{term}' for `#{attribute}' property is expected to be a URI, but it is a #{term.class}"
+          end
         end
       end
     end
